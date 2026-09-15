0879b1c676a49e5a4203844a310bd0792d0f119f
Lawrence Cachero <lawrence.cachero@mgenesis.com>
2026-09-15 16:13:16 +0800

    Added search functionality

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("AI Hub Loaded Successfully");
+const searchInput = document.getElementById('searchInput');
+
+searchInput.addEventListener('keyup', function () {
+
+    const searchTerm = searchInput.value.toLowerCase();
+
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+
+        const text = card.innerText.toLowerCase();
+
+        if (text.includes(searchTerm)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+
+    });
+
+});
